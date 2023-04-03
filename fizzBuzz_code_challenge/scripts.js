3a9f4fc906682257afbd764f0ecc11203e2bda8a
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-04-03 13:58:28 +0200

    First commit

---
+++ fizzBuzz_code_challenge/scripts.js
@@ -0,0 +1,8 @@
+//loop will run until i less than or equals to 100
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzBuzz");
+  } else if (i % 3 === 0) console.log("fizz");
+  else if (i % 5 === 0) console.log("buzz");
+  else console.log(i);
+}
