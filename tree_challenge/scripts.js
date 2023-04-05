98edbc8342ef97ed55ddc51f72721924f994c9aa
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-04-05 10:13:47 +0200

    Completed tree challenge

---
+++ tree_challenge/scripts.js
@@ -0,0 +1,9 @@
+let treeString = "";
+// Set a loop that starts from 1 and runs as long as i is
+// less than 5
+for (let i = 1; i < 5; i++) {
+  //with each iteration "*" is added to the tree string
+  treeString += "*";
+  //the resulting string is consoled each time.
+  console.log(treeString);
+}
